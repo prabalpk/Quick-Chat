2e3db0bd6e75908fb50aded49f5c4fdcc7d6c01d
prabalpk <prabalpk12@gmail.com>
2025-09-24 12:05:10 +0530

    update_2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import LOginPage from './pages/LOginPage'
 import ProfilePage from './pages/ProfilePage'
+import LOginPage from './pages/LoginPage'
 
 const App = () => {
   return (
+    <div className="h-screen bg-[url('/src/assets/bgImage.svg')] bg-cover bg-center">
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<LOginPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+  </Routes>
+</div>
 
-    
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
-      <Routes>
-        <Route path ='/'element={<HomePage />}/>
-        <Route path ='/login'element={<LOginPage />}/>
-        <Route path ='/profile'element={<ProfilePage />}/>
 
-       
-      </Routes>
-    </div>
   )
 }
 
 export default App
+
+
+
