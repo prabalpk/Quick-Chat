c0124bedeeb9037a6a55d62755a7444cad1bda0f
prabalpk <prabalpk12@gmail.com>
2025-09-24 19:39:47 +0530

    update_3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
-import LOginPage from './pages/LoginPage'
+
 
 const App = () => {
   return (
-    <div className="h-screen bg-[url('/src/assets/bgImage.svg')] bg-cover bg-center">
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LOginPage />} />
-    <Route path="/profile" element={<ProfilePage />} />
-  </Routes>
-</div>
-
-
+    <div className="bg-[url('/src/assets/bgImage.svg')] bg-center">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
 export default App
 
+//className="h-screen bg-[url('/src/assets/bgImage.svg')] bg-cover bg-center"
 
-
+//"bg-[url('/src/assets/bgImage.svg')] bg-center"
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import SideBar from '../copmonents/SideBar'
-import ChatContainer from '../copmonents/ChatContainer'
-import RightSidebar from '../copmonents/RightSidebar'
-
+import React, { useState } from 'react'
+import SideBar from '../components/SideBar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
+  const [selectedUser, setSelectedUser] = useState(false)
+
   return (
-    <div className='border w-full h-screen sm:px-screen sm:py-[5%]'>
-      <div>
-        <SideBar />
-        <ChatContainer />
-        <RightSidebar />
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl 
+        overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 
+            'md:grid-cols-[1fr_1.5fr_1fr] x1:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'
+        }`}
+      >
+        <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       </div>
     </div>
   )
 }
 
 export default HomePage
-//className='border w-full h-screen sm:px-screen sm:py-[5%]'
